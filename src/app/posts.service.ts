3095111f922401fdc7cc2a17ede12e7dae6e0803
Liudmyla Melnychuk <kverchi24@gmail.com>
2019-04-04 12:55:41 +0200

    Read base url property in services

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -3,25 +3,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, Observer, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Post } from './post';
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PostsService {
+    baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http:HttpClient) {
+    }
 
-  getPostsPage(currentPage: any): Observable<any> {
-    return this.http.get('http://localhost:8080/posts/page/'+currentPage)
-        .pipe(
-            tap(val => console.log(`Calling getAllPosts method. The response: ${val}`)),
-            map(res => {
-              return res;
-            }),
-            catchError((error: any) => {
-              /*console.error(error);*/
-              return of(error);
-            })
-        );
-  }
+    getPostsPage(currentPage:any):Observable<any> {
+        return this.http.get(this.baseUrl + '/posts/page/' + currentPage)
+            .pipe(
+                tap(val => console.log(`Calling getAllPosts method. The response: ${val}`)),
+                map(res => {
+                    return res;
+                }),
+                catchError((error:any) => {
+                    /*console.error(error);*/
+                    return of(error);
+                })
+            );
+    }
 }
--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -3,25 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, Observer, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { RegistrationForm } from './registration-form';
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class RegistrationService {
+    baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http:HttpClient) {
+    }
 
-  register(registrationForm: RegistrationForm) : Observable<any> {
-    return this.http.post("http://localhost:8080/user/register", registrationForm)
-        .pipe(
-            tap(res => console.log(`results: ${res}`)),
-            map(res => {
-              return res;
-            }),
-            catchError((error: any) => {
-              console.log(error);
-              return of(error);
-            })
-        );
-  }
+    register(registrationForm:RegistrationForm):Observable<any> {
+        return this.http.post(this.baseUrl + "/user/register", registrationForm)
+            .pipe(
+                tap(res => console.log(`results: ${res}`)),
+                map(res => {
+                    return res;
+                }),
+                catchError((error:any) => {
+                    console.log(error);
+                    return of(error);
+                })
+            );
+    }
 }
