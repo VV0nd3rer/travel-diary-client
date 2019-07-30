c3b39bf6d560d053c548163ac508cf4d765b737c
Liudmyla Melnychuk <kverchi24@gmail.com>
2019-07-30 15:00:10 +0200

    Added validation with Angular reactive forms for save post form

--- src/app/save-post/save-post.component.ts
+++ src/app/save-post/save-post.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {PostsService} from "../services/posts.service";
 
 @Component({
-  selector: 'app-save-post',
-  templateUrl: './save-post.component.html',
-  styleUrls: ['./save-post.component.css']
+    selector: 'app-save-post',
+    templateUrl: './save-post.component.html',
+    styleUrls: ['./save-post.component.css']
 })
 export class SavePostComponent implements OnInit {
-  constructor(private postService: PostsService) { }
+    savePostForm: any;
 
-  ngOnInit() {
-
-  }
-  getInitData() {
-    var service = this.postService;
-    return {
-      images_upload_handler: function(blobInfo, success, failure) {
-        service.uploadImage(blobInfo, success, failure);
-      }
+    constructor(private postService:PostsService,
+                private formBuilder:FormBuilder) {
+        this.savePostForm = this.formBuilder.group({
+            'description': ['', [Validators.required, Validators.minLength(10)]],
+            'text': ['', [Validators.required, Validators.minLength(40)]]
+        });
     }
-  }
+
+    ngOnInit() {
+
+    }
+
+    getInitData() {
+        var service = this.postService;
+        return {
+            images_upload_handler: function (blobInfo, success, failure) {
+                service.uploadImage(blobInfo, success, failure);
+            }
+        }
+    }
 }
