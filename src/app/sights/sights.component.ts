a4ab42cc45e096eed0e7dd074e17e7e8ea16ab5f
Liudmyla Melnychuk <kverchi24@gmail.com>
2019-08-01 02:06:06 +0200

    Added Mapbox provider of online maps
    The Leaflet library uses the Mapbox, which takes data from the OpenStreetMap source.

--- src/app/sights/sights.component.ts
+++ src/app/sights/sights.component.ts
@@ -8,8 +8,16 @@ import { latLng, LatLng, tileLayer } from 'leaflet';
 })
 export class SightsComponent implements OnInit {
   options = {
+    name: 'Mapbox',
+    enabled: true,
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1Ijoia3ZlcmNoaSIsImEiOiJjanlyd2NncnMwOTdtM2NwNThuMHVreGpzIn0.GiLeL75YtFDxkEvvBOw5lQ',
+      })
     ],
     zoom: 5,
     center: [ 46.879966, -121.726909 ]
