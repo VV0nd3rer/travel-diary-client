c5484b0e289fb8404f182dc3ff4682c359ee1574
Liudmyla Melnychuk <kverchi24@gmail.com>
2019-08-02 18:07:19 +0200

    Fixed Typescript complaining and added samples of sights' markers
    Typescript was complaining because it's missing the typing information about layer ID (Similar issue: https://github.com/Asymmetrik/ngx-leaflet/issues/198#issuecomment-445956999). Loaded layer with sample markers.

--- src/app/sights/sights.component.ts
+++ src/app/sights/sights.component.ts
@@ -1,31 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, LatLng, tileLayer } from 'leaflet';
+import { latLng, LatLng, tileLayer, Layer, marker, icon } from 'leaflet';
 
 @Component({
-  selector: 'app-sights',
-  templateUrl: './sights.component.html',
-  styleUrls: ['./sights.component.css']
+    selector: 'app-sights',
+    templateUrl: './sights.component.html',
+    styleUrls: ['./sights.component.css']
 })
 export class SightsComponent implements OnInit {
-  options = {
-    name: 'Mapbox',
-    enabled: true,
-    layers: [
-      tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1Ijoia3ZlcmNoaSIsImEiOiJjanlyd2NncnMwOTdtM2NwNThuMHVreGpzIn0.GiLeL75YtFDxkEvvBOw5lQ',
-      })
-    ],
-    zoom: 5,
-    center: [ 46.879966, -121.726909 ]
-  };
-  constructor() { }
+    sights: any = [
+        [-40.99497, 174.50808],
+        [-41.30269, 173.63696],
+        [-41.49413, 173.5421],
+        [-40.98585, 174.50659]
+    ];
+    markers:Layer[] = [];
 
-  ngOnInit() {
+    options = {
+        name: 'Mapbox',
+        enabled: true,
+        layers: [
+            tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+                {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    id: 'mapbox.streets',
+                    maxZoom: 18,
+                    accessToken: 'pk.eyJ1Ijoia3ZlcmNoaSIsImEiOiJjanlyd2NncnMwOTdtM2NwNThuMHVreGpzIn0.GiLeL75YtFDxkEvvBOw5lQ'
+                } as any)
+        ],
+        zoom: 9,
+        center: [-41.49413, 173.5421]
+    };
 
-  }
 
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        this.addMarker();
+    }
+
+    addMarker() {
+        for (let m of this.sights) {
+            console.log(m);
+            const newMarker = marker(
+                m, {
+                    icon: icon({
+                        iconSize: [25, 41],
+                        iconAnchor: [13, 41],
+                        iconUrl: 'assets/marker-icon.png',
+                        shadowUrl: 'assets/marker-shadow.png'
+                    })
+                })
+            this.markers.push(newMarker);
+        }
+    }
 }
